2fd39e42f4641af354424209d5bf78d636d43a1d
Cynthia Wu <cynthia21wu@gmail.com>
2020-09-26 15:53:21 -0700

    Adding content for the highscores page

--- script.js
+++ script.js
@@ -6,25 +6,25 @@
 // C. End of quiz/record highscore
 // I. New game and clear highscores question
 
-
+    //Your entire JS code here
 // Call the DOM
+var tryagainEl = document.querySelector("#tryagain-btn").addEventListener("click", tryAgain);
 var startButton = document.querySelector("#start-btn").addEventListener("click", startQuiz);
 var timerEl = document.querySelector("#timer");
 var introEl = document.querySelector("#introContainer");
 var questionContainerEl = document.querySelector("#questionContainer");
 var questionEl = document.querySelector("#question"); 
-// var actionEl = document.querySelector("#action")
 var answerbtnEl = document.querySelectorAll(".answer-btn");
 var judgeEl = document.querySelector("#judge");
 var alldoneEl = document.querySelector("#alldoneContainer");
 var finalscoreEl = document.querySelector("#final-score")
 var gameoverEl = document.querySelector("#gameoverContainer");
-var tryagainEl = document.querySelector("#tryagain-btn").addEventListener("click", tryAgain);
 
 var currentQuestionIndex = 0;
+var gameFinished = false;
 
 // Question Array
-var questionsArr = [
+const questionsArr = [
     {
         question: "What does 'HTML' stand for?",
         answers: [
@@ -84,7 +84,7 @@ var questionsArr = [
             "A scripting language", 
             "None of the above"
         ],
-        correct: "A JavaScript library"
+        correct: "A CSS framework"
     },
     {
         question: "Which of these data types is a Boolean?",
@@ -110,6 +110,7 @@ var questionsArr = [
 
 //Function to set and start the timer. 
 var timeLeft = 75;
+var timeInterval;
 
 function setTimer() {
 
@@ -120,15 +121,17 @@ function setTimer() {
     
     if (timeLeft <= -1) {
         //Function to open up the Highscores page
-        timeLeft = 0;
         clearInterval(timeInterval);
         gameOver();
-        
-    }   
+    }  
+
+    else if (gameFinished === true) {
+        clearInterval(timeInterval);
+        quizDone();
+    }
+
     }, 1000);
 }
-
-currentQuestionIndex = 0;
 
 //Function to dynamically populate the question content into hidden div. 
 function setQuestion() {
@@ -137,7 +140,7 @@ function setQuestion() {
     for (let i = 0; i < questionsArr.length; i++) {
         questionEl.textContent = questionsArr[currentQuestionIndex].question;
 
-        for(let j = 0; j < questionsArr[currentQuestionIndex].answers.length; j++) {
+        for (let j = 0; j < questionsArr[currentQuestionIndex].answers.length; j++) {
         answerbtnEl[j].value = questionsArr[currentQuestionIndex].answers[j];
         answerbtnEl[j].textContent = questionsArr[currentQuestionIndex].answers[j];
         }
@@ -163,17 +166,15 @@ function quizDone() {
     if (currentQuestionIndex < questionsArr.length) {
         setTimeout(setQuestion,1000);
     }
-    else{
+    else {
         questionContainerEl.classList.add("hide");
         alldoneEl.classList.remove("hide");
-
+        gameFinished = true;
+        finalscoreEl.textContent = timeLeft;
+        timerEl.textContent = timeLeft;
+        // There is  a lag with this. Should I push the time left into an array in my interval function and then retrive that here? 
     }
-    // else {
-    // setTimeout(changePage,1000);
-    // }
 }
-
-
 
 answerbtnEl[0].addEventListener("click", selectAnswer);
 answerbtnEl[1].addEventListener("click", selectAnswer);
@@ -207,17 +208,21 @@ function selectAnswer(event) {
         judgeEl.classList.remove("hide");
         quizDone();
     }
-
 }
 
 function gameOver() {
+    timerEl.textContent = 0;
     questionContainerEl.classList.add("hide");
     gameoverEl.classList.remove("hide");
     alldoneEl.classList.add("hide");
 }
 
+// Function to reset to code quiz. Not sure if this is the best way to do this. 
+
 function tryAgain() {
-    window.location.reload();
+    location.replace("codequiz.html");
+
 
 }
+
 
---
+++ script2.js
@@ -0,0 +1,6 @@
+var tryagainEl2 = document.querySelector("#tryagain-btn2").addEventListeneraddEventListener("click", tryAgain2);
+
+function tryAgain2() {
+    window.open("codequiz.html");
+
+}
